f75305e937a0ba577461459c223437101e01f496
abhinavinteldata <abhinav@inteldata.org>
2022-02-21 22:33:37 +0530

    updated code

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+  Text, View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Society</Text>
+    </View>
+  );
+};
+
+export default App;
